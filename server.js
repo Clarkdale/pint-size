c13cf5998d03a2437aa1c71292a6e927aedde589
Clarkdale <clarkdale@email.arizona.edu>
2020-05-26 16:12:49 -0700

    Add utility function

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4003;
 
 app.use(cors());
 
-var map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var base61Map = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 var service = "https://smllr.herokuapp.com";
 
@@ -18,10 +18,10 @@ app.get('/:urlId', (request, response) => {
     let result = 0;
 
     for (let i = 0, j = init.length - 1; i < init.length && j >= 0; i++, j--) {
-        curr = map.indexOf(init[i]);
+        curr = base61Map.indexOf(init[i]);
         result += (curr * Math.pow(61, j));
     }
-    fetch(`${service}/getUrl/${result}`)
+    fetch(`${service}/getUrl/${convertBase10(request.params.urlId)}`)
         .then(res => {
             return res.json();
         })
@@ -31,6 +31,15 @@ app.get('/:urlId', (request, response) => {
         .catch(e => response.status(500).send('The link could not be found'));
         
 });
+
+function convertBase10(base61Number) {
+    let retVal = 0;
+
+    for (let i = 0, j = base61Number.length - 1; i < base61Number.length && j >= 0; i++, j--) {
+        curr = base61Map.indexOf(base61Number[i]);
+        retVal += (curr * Math.pow(61, j));
+    }
+}
 
 // start the server
 app.listen(port, () => console.log('Listening on port ' + port));
